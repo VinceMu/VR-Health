7809a181ed8c1c656ce00babbe255f2a29ae024f
Your Name <you@example.com>
2018-06-02 19:22:05 +1000

    for brian

---
+++ Assets/log.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class log : MonoBehaviour {
+
+    public Transform pos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        System.Console.WriteLine(pos.position.x.ToString() + " " + pos.position.y.ToString() + " " + pos.position.z.ToString());
+	}
+}
